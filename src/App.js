05d7e5ebcff2eaa9858bf599e421528641201e03
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-07-08 12:05:08 +0530

    Q1. a: Create a Button that counts no of clicks and displays on button itself.  Now display odd or even next to the button based on the value of count.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h6>Task 1</h6>
+      <Counter/>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      text1: "odd",
+      text2: "even",
+      text: "",
+    };
+  }
+
+  Count_handler = () => {
+    console.log(this.state.count);
+    this.setState({
+      count: this.state.count + 1,
+    });
+    if (this.state.count % 2 === 0) {
+    //   alert("hello");
+      this.setState({
+        text: this.state.text2,
+      });
+      console.log(this.text2);
+    } else {
+      this.setState({
+        text: this.state.text1,
+      });
+    }
+    console.log(this.state.text);
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.Count_handler}>
+          click {this.state.count} times
+        </button>
+        <div>{this.state.text}</div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
